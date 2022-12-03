c54f352b48b549f7c8e1e047a69c029934dfd6bb
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-02 22:53:49 -0300

    novos

---
+++ Scripts Download/24_desafio.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome “Santo”.
+
+cid = str(input("Insira o nome da cidade: ")).strip().split()
+print(cid[0].upper() == 'SANTO')
