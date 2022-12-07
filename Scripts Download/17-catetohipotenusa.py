6e4f34abe16ba3518884384c95c96fac434fcd60
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-07 19:04:42 -0300

    Update and rename 17_desafio.py to 17-catetohipotenusa.py

--- Scripts Download/17-catetohipotenusa.py
+++ Scripts Download/17-catetohipotenusa.py
@@ -1,3 +1,4 @@
+# Desafio 17 - Cateto e Hipotenusa
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo,
 # calcule e mostre o comprimento da hipotenusa
 
