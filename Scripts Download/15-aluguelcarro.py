80bf0359f7f9d15f1ddae83ca54f978ca85ce570
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-06 21:31:44 -0300

    Update and rename 15_desafio.py to 15-aluguelcarro.py

--- Scripts Download/15-aluguelcarro.py
+++ Scripts Download/15-aluguelcarro.py
@@ -1,6 +1,7 @@
-#Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e
-#a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar,
-#sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por Km rodado.
+# Desafio 15
+# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e
+# a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar,
+# sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por Km rodado.
 
 km=float(input("Insira a quantidade de km percorridos: "))
 dias=int(input("Insira a quantidade de dias de aluguel do veículo: "))
