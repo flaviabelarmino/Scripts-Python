32e3d4c7368f23ce67a66d0537c78290ec725598
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-06 21:30:01 -0300

    Update and rename 13_desafio.py to 13-aumentsalfunc.py

--- Scripts Download/13-aumentsalfunc.py
+++ Scripts Download/13-aumentsalfunc.py
@@ -1,4 +1,5 @@
-#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
+# Desafio 13
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
 
 salfunc=float(input("Insira o salário do funcionário R$: "))
 aumento=(salfunc*15)/100
