f5cab8d373081d0daf78cb01d87b7c79cec3225a
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-06 21:29:21 -0300

    Update and rename 12_desafio.py to 12-produtoporcent.py

--- Scripts Download/12-produtoporcent.py
+++ Scripts Download/12-produtoporcent.py
@@ -1,4 +1,5 @@
-#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+# Desafio 12
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
 p=float(input("Insira o preço do produto R$: "))
 desconto=(p*5)/100
