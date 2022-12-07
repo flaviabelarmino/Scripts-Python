a98189e29b7d598bdeb85282d155f77af0b5ecbe
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-06 21:31:02 -0300

    Update and rename 14_desafio.py to 14-convertertemperatura.py

--- Scripts Download/14_desafio.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF.
-
-c=float(input("Insira a temperatura em °C: "))
-f=(c*9/5)+32
-print(f"Convertendo: {c}C° para {f}°F")
