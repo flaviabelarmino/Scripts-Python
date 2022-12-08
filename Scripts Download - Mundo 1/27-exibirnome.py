215cb74aa52a96e204b968bceefb5d161e841fd6
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-08 18:29:29 -0300

    Update and rename 27_desafio.py to 27-exibirnome.py

--- Scripts Download - Mundo 1/27-exibirnome.py
+++ Scripts Download - Mundo 1/27-exibirnome.py
@@ -1,4 +1,5 @@
-#Faça um programa que leia o nome completo de uma pessoa,
+# Desafio 27 - Exibir nome
+# Faça um programa que leia o nome completo de uma pessoa,
 # mostrando em seguida o primeiro e o último nome separadamente.
 # Ex: Ana Maria de Souza
 
