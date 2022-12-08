99a705ac74ee8e7b5d0abc6f55de76f398ea996c
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-08 18:26:58 -0300

    Update and rename 25_desafio.py to 25-verificarnome.py

---
+++ Scripts Download - Mundo 1/25-verificarnome.py
@@ -0,0 +1,5 @@
+# Desafio 25 - Verificar nome
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem “Silva” no nome.
+
+nome = str(input("Insira seu nome completo: ")).strip().lower().split()
+print("Seu nome tem Silva? ",'silva' in nome)
--- Scripts Download - Mundo 1/25_desafio.py
+++
@@ -1,4 +0,0 @@
-# – Crie um programa que leia o nome de uma pessoa e diga se ela tem “Silva” no nome.
-
-nome = str(input("Insira seu nome completo: ")).strip().lower().split()
-print("Seu nome tem Silva? ",'silva' in nome)
