d1e0f93b953404188a3087c269a34c03605009a7
flaviabelarmino <114199834+flaviabelarmino@users.noreply.github.com>
2022-12-08 18:25:12 -0300

    Update and rename 23_desafio.py to 23-separardigitos.py

--- Scripts Download - Mundo 1/23-separardigitos.py
+++ Scripts Download - Mundo 1/23-separardigitos.py
@@ -1,4 +1,5 @@
-#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+# Desafio 23 - Separar digitos
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 # Ex: digite o nº 1834. Unidade: 4; Dezena: 3, Centena: 8, Milhar 1
 
 num = int(input("Digite um número: "))
